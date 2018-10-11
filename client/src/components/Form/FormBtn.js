7de0dc5e3d6b660eeaa47ea3fa31280eb9664222
Elijah Kotyluk <elijah@elijahkotyluk.com>
2018-10-11 13:35:48 -0700

    Change some css colors to match color scheme.

--- client/src/components/Form/FormBtn.js
+++ client/src/components/Form/FormBtn.js
@@ -1,4 +1,4 @@
 import React from "react";
 
 export const FormBtn = props =>
-  <button type="button" className="btn btn-outline-primary float-right" {...props}>Search</button>;
+  <button type="button" className="btn btn-outline-default float-right" {...props}>Search</button>;
